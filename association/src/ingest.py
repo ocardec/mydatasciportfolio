82e0c957251598b60a7801267c0a1182a730b572
Oscar Cardec <61342187+ocardec@users.noreply.github.com>
2023-12-09 22:46:59 -0500

    Update ingest.py

--- association/src/ingest.py
+++ association/src/ingest.py
@@ -1,1 +1,21 @@
 
+# URL https://archive.ics.uci.edu/dataset/27/credit+approval 
+
+pip install ucimlrepo
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+credit_approval = fetch_ucirepo(id=27) 
+  
+# data (as pandas dataframes) 
+X = credit_approval.data.features 
+y = credit_approval.data.targets 
+  
+# metadata 
+print(credit_approval.metadata) 
+  
+# variable information 
+print(credit_approval.variables) 
+
+
+
